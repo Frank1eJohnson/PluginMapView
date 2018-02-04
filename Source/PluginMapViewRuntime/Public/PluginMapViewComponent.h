48f6730024d1780af6872406dc3b692e47b5fc2f
Zhongqi Shan <60977360+Sabrave@users.noreply.github.com>
2018-02-05 01:10:17 +0800

    Fixed missing category name in PluginMapViewComponent

--- Source/PluginMapViewRuntime/Public/PluginMapViewComponent.h
+++ Source/PluginMapViewRuntime/Public/PluginMapViewComponent.h
@@ -71,7 +71,7 @@ protected:
 protected:
 	
 	/** The street map we're representing */
-	UPROPERTY( EditAnywhere )
+	UPROPERTY( EditAnywhere, Category="PluginMapView" )
 	class UPluginMapView* PluginMapView;
 
 
